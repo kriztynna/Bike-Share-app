1e22ca8a065eb9cf69b7e6510061c0284b2bc610
Cristina <cristina.colon+git@gmail.com>
2013-06-30 10:14:06 -0400

    removed comments
    & compressed code a bit

--- bikeshareapp.py
+++ bikeshareapp.py
@@ -44,14 +44,10 @@ class MainPage(webapp2.RequestHandler):
 class ShowStationData(MainPage):
         def render_show_data(self):
 		self.write("hello here is the data you have stored!<br><br>")
-		#stations = db.GqlQuery("SELECT * FROM StationInfo ORDER BY station_id DESC")
 		for station in db.GqlQuery("SELECT station_id, name FROM StationInfo ORDER BY station_id ASC"):
-                #for station in stations:
                         status = StationStatus.all().filter('station_id =',station.station_id).order('-date_time').get()
-                        avail = status.availableBikes
-                        message = station.name+' has '+str(avail)+' available bikes'+'<br><br>'
+                        message = station.name+' has '+str(status.availableBikes)+' available bikes'+'<br><br>'
                         self.write(message)
-
 	def get(self):
 		self.render_show_data()
 
